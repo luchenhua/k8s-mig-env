e8ea2a2b1e71259f7065c29cc8a6bb5d761af6cd
Chenhua LU <luchenhua@icloud.com>
2021-09-14 06:28:18 +0800

    <fix>: modify data script.

--- init.sql
+++ init.sql
@@ -9,5 +9,9 @@ CREATE TABLE tbl_user (
     opt_in SMALLINT,
     is_deleted SMALLINT,
     CONSTRAINT tbl_user_pkey PRIMARY KEY (id),
-    CONSTRAINT "tbl_subscription_unique" UNIQUE (mobile, uuid)
+    CONSTRAINT tbl_user_unique UNIQUE (mobile, uuid)
 );
+ALTER TABLE tbl_user
+SET UNLOGGED;
+ALTER TABLE tbl_user DROP CONSTRAINT tbl_user_pkey;
+ALTER TABLE tbl_user DROP CONSTRAINT tbl_user_unique;
--- init_data.sh
+++ init_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-datagen -script datagen.script --driver postgres --conn postgres://test001:test001@localhost:5432/msghub?sslmode=disable
+go get -u github.com/codingconcepts/datagen && datagen -script datagen.script --driver postgres --conn postgres://test001:test001@localhost:5432/msghub?sslmode=disable
