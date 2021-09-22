09354b4ba3a98190da0e384a455e272d941a5d63
Chenhua Lu <alex.lu@decathlon.com>
2021-09-22 19:50:18 +0800

    <feat>: add multiple tables for test

--- init.sql
+++ init.sql
@@ -15,3 +15,14 @@ CREATE TABLE tbl_user (
 -- SET UNLOGGED;
 -- ALTER TABLE tbl_user DROP CONSTRAINT tbl_user_pkey;
 -- ALTER TABLE tbl_user DROP CONSTRAINT tbl_user_unique;
+CREATE TABLE tbl_employee (
+    id SERIAL NOT NULL,
+    uuid VARCHAR(50) NULL,
+    first_name VARCHAR(20) NULL,
+    last_name VARCHAR(20) NULL,
+    gender VARCHAR(10) NULL,
+    mobile VARCHAR(20) NULL,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT tbl_employee_pkey PRIMARY KEY (id),
+    CONSTRAINT tbl_employee_unique UNIQUE (mobile, uuid)
+);
